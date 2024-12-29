1651dae249295b6bd4e9f03e074071fa552a7a08
Ahmed Ehab <ahmed.ehab5010@gmail.com>
2024-12-29 21:01:59 +0200

    Add simple config and arg parsing

--- cmd/glsync/main.go
+++ cmd/glsync/main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"encoding/base64"
+	"encoding/json"
+	"flag"
+	"log"
+	"net"
+	"net/url"
+	"strings"
+
+	"github.com/ahmed-e-abdulaziz/gh-leet-sync/config"
+)
+
+const (
+	lcCookieCmd = "lc-cookie"
+	repoUrlCmd  = "repo-url"
+)
 
 func main() {
-	fmt.Println("Hello There2!")
+	cfg := config.Config{}
+	flag.StringVar(&cfg.LCookie, lcCookieCmd, "", "The cookie of your LeetCode session, refer to the README.md for more info")
+	flag.StringVar(&cfg.RepoUrl, repoUrlCmd, "", "Github repo's url to push LC submissions to")
+	flag.Parse()
+	if !isValidCookie(cfg.LCookie) {
+		log.Fatalf("Invalid leet code session cookie provided, use -%v option to provide your leetcode cookie", lcCookieCmd)
+	}
+	if !isGitHubURL(cfg.RepoUrl) {
+		log.Fatalf("Invalid github repo url provided provided, use -%v option to provide your github repo url ", repoUrlCmd)
+	}
+	log.Println("Input parsed successfully.")
+}
+
+func isValidCookie(cookie string) bool {
+	payload, err := base64.StdEncoding.DecodeString(strings.Split(cookie, ".")[1])
+	if err != nil {
+		return false
+	}
+	return json.Unmarshal([]byte(payload), &json.RawMessage{}) == nil
+}
+
+func isGitHubURL(input string) bool {
+	u, err := url.Parse(input)
+	if err != nil {
+		return false
+	}
+	host := u.Host
+	if strings.Contains(host, ":") {
+		host, _, err = net.SplitHostPort(host)
+		if err != nil {
+			return false
+		}
+	}
+	return host == "github.com"
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ahmed-e-abdulaziz/gh-leet-sync
 
 go 1.23.3
+
+require github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
