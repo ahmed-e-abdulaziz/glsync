8d31659370978bb66cbd09ffaa88ef929720889d
Ahmed Ehab <ahmed.ehab5010@gmail.com>
2024-12-30 22:59:20 +0200

    Add fetching leetcode submission code and language

--- handler/handler.go
+++ handler/handler.go
@@ -19,9 +19,45 @@ func NewHandler(cfg config.Config, leetcode leetcode.LeetCode) Handler {
 
 func (h Handler) Execute() {
 	questions := h.leetcode.FetchQuestions()
+	submissionsOverview := map[string]leetcode.SumbissionOverview{} // question title slug, the submission overview
 	for _, q := range questions {
-		fmt.Print(q.FrontendId)
-		fmt.Print(" " + q.Title + "\n")
+		submissionsOverview[q.TitleSlug] = h.leetcode.FetchSubmissionOverview(q.TitleSlug)
+		s := submissionsOverview[q.TitleSlug]
+		code := h.leetcode.FetchSubmissionCode(s.Id)
+		fileName := h.buildFileName(q.FrontendId, q.Title, s.Lang)
+		folderName := fmt.Sprintf("%v %v", q.FrontendId, q.Title)
+		fmt.Printf(code, fileName, folderName, q.LastSubmittedAt)
 	}
+
 	log.Println("Not implemented yet")
+}
+
+func (Handler) buildFileName(id, titleSlug, lang string) string {
+	langFileExtension := map[string]string{
+		"cpp":        "cpp",
+		"java":       "java",
+		"python":     "py",
+		"python3":    "py",
+		"mysql":      "sql",
+		"mssql":      "sql",
+		"oraclesql":  "sql",
+		"c":          "c",
+		"csharp":     "cs",
+		"javascript": "js",
+		"typescript": "ts",
+		"bash":       "sh",
+		"php":        "php",
+		"swift":      "swift",
+		"kotlin":     "kt",
+		"dart":       "dart",
+		"golang":     "go",
+		"ruby":       "rb",
+		"scala":      "scala",
+		"rust":       "rs",
+		"racket":     "rkt",
+		"erlang":     "erl",
+		"elixir":     "ex",
+		"postgresql": "sql",
+	}
+	return fmt.Sprintf("%s%s.%s", id, titleSlug, langFileExtension[lang])
 }
--- leetcode/leetcode.go
+++ leetcode/leetcode.go
@@ -3,7 +3,9 @@ package leetcode
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"time"
 
@@ -30,10 +32,51 @@ func (lc LeetCode) FetchQuestions() []Question {
 		},
 		"operationName": "userProgressQuestionList"
 	}`
-	bodyBytes, _ := lc.queryGraphql(query)
+	bodyBytes, err := lc.queryGraphql(query)
+	if err != nil {
+		log.Fatal("Encountered an error while fetching user questions", err)
+	}
 	body := &RequestBody[UserProgressQuestionListData]{}
 	json.Unmarshal(bodyBytes, body)
 	return body.Data.QuestionsList.Questions
+}
+
+func (lc LeetCode) FetchSubmissionOverview(titleSlug string) SumbissionOverview {
+	query := fmt.Sprintf(`{
+		"query": "\n    query submissionList($offset: Int!, $limit: Int!, $lastKey: String, $questionSlug: String!, $lang: Int, $status: Int) {\n  questionSubmissionList(\n    offset: $offset\n    limit: $limit\n    lastKey: $lastKey\n    questionSlug: $questionSlug\n    lang: $lang\n    status: $status\n  ) {\n    lastKey\n    hasNext\n    submissions {\n      id\n      title\n      titleSlug\n      status\n      statusDisplay\n      lang\n      langName\n      runtime\n      timestamp\n      url\n      isPending\n      memory\n      hasNotes\n      notes\n      flagType\n      frontendId\n      topicTags {\n        id\n      }\n    }\n  }\n}\n    ",
+		"variables": {
+			"questionSlug": "%v",
+			"offset": 0,
+			"limit": 1,
+			"lastKey": null
+		},
+		"operationName": "submissionList"
+	}`, titleSlug)
+
+	bodyBytes, err := lc.queryGraphql(query)
+	if err != nil {
+		log.Fatal("Encountered an error while fetching submssion overview", err)
+	}
+	body := &RequestBody[SubmissionListData]{}
+	json.Unmarshal(bodyBytes, body)
+	return body.Data.SubmissionList.Submissions[0] // we only need the lastest submission
+}
+
+func (lc LeetCode) FetchSubmissionCode(id string) string {
+	query := fmt.Sprintf(`{
+			"query": "\n    query submissionDetails($submissionId: Int!) {\n  submissionDetails(submissionId: $submissionId) {\n    runtime\n    runtimeDisplay\n    runtimePercentile\n    runtimeDistribution\n    memory\n    memoryDisplay\n    memoryPercentile\n    memoryDistribution\n    code\n    timestamp\n    statusCode\n    user {\n      username\n      profile {\n        realName\n        userAvatar\n      }\n    }\n    lang {\n      name\n      verboseName\n    }\n    question {\n      questionId\n      titleSlug\n      hasFrontendPreview\n    }\n    notes\n    flagType\n    topicTags {\n      tagId\n      slug\n      name\n    }\n    runtimeError\n    compileError\n    lastTestcase\n    codeOutput\n    expectedOutput\n    totalCorrect\n    totalTestcases\n    fullCodeOutput\n    testDescriptions\n    testBodies\n    testInfo\n    stdOutput\n  }\n}\n    ",
+			"variables": {
+				"submissionId": %v
+			},
+			"operationName": "submissionDetails"
+		}`, id)
+	bodyBytes, err := lc.queryGraphql(query)
+	if err != nil {
+		log.Fatal("Encountered an error while fetching submssion overview", err)
+	}
+	body := &RequestBody[SubmissionDetailsData]{}
+	json.Unmarshal(bodyBytes, body)
+	return body.Data.Details.Code
 }
 
 func (lc LeetCode) queryGraphql(query string) ([]byte, error) {
@@ -82,4 +125,24 @@ type Question struct {
 	LastSubmittedAt time.Time `json:"lastSubmittedAt"`
 	QuestionStatus  string    `json:"questionStatus"`
 	LastResult      string    `json:"lastResult"`
+}
+
+type SubmissionListData struct {
+	SubmissionList SubmissionList `json:"questionSubmissionList"`
+}
+type SubmissionList struct {
+	Submissions []SumbissionOverview `json:"submissions"`
+}
+
+type SumbissionOverview struct {
+	Id   string `json:"id"`
+	Lang string `json:"lang"`
+}
+
+type SubmissionDetailsData struct {
+	Details SubmissionDetails `json:"submissionDetails"`
+}
+
+type SubmissionDetails struct {
+	Code string `json:"code"`
 }
